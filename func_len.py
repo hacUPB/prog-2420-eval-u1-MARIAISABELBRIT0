2412679ea2a80c0220e299d581313047050e382f
maria isabel <maria.brito@upb.edu.co>
2024-09-03 16:03:44 -0500

    Se borran archivos no necesarios

--- func_len.py
+++
@@ -1,9 +0,0 @@
-lista = [4,54, 66, 77, 32, 11, 12, 34, 34, 56, 7, 5, 3, 6, 77, 44, 2, 2, 45, 78]
-cont= 0
-for elem in lista:
-    cont += 1
-    print (f"la lista tiene {cont} elementos") 
-
-#comprobacion
-num_elementos = len(lista)
-print (f"usando la funcion len nos da {num_elementos}")
